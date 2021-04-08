95425d9e172fc211a6c545504ec8c470cc4a5622
dangnam739 <kieudangnam99@gmail.com>
2021-04-08 13:59:28 +0000

    add data and index.html

---
+++ src/data.js
@@ -0,0 +1,33 @@
+/* 
+https://www.iconfinder.com/icons/1243689/call_phone_icon
+Creative Commons (Attribution 3.0 Unported);
+https://www.iconfinder.com/Makoto_msk */
+
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Mèo Ba Tư",
+    img: "img/product-1.png",
+    price: 10,
+    company: "Iran",
+    info:
+      "",
+    inCart: true,
+    count: 0,
+    total: 0
+  },
+  
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "",
+  img: "img/product-1.png",
+  price: 10,
+  company: "",
+  info:
+    "",
+  inCart: false,
+  count: 0,
+  total: 0
+};
