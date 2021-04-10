74a64fa22e97908d8dd3e267eefc0716c4dbd4af
dangnam739 <kieudangnam99@gmail.com>
2021-04-10 13:54:09 +0000

    update data

--- src/data.js
+++ src/data.js
@@ -1,33 +1,119 @@
-/* 
-https://www.iconfinder.com/icons/1243689/call_phone_icon
-Creative Commons (Attribution 3.0 Unported);
-https://www.iconfinder.com/Makoto_msk */
 
 export const storeProducts = [
   {
     id: 1,
-    title: "Mèo Ba Tư",
-    img: "img/product-1.png",
+    type: "Mèo Aegean Hi Lạp",
+    img: "img/cat-1.png",
     price: 10,
-    company: "Iran",
+    origin: "Greek",
     info:
-      "",
-    inCart: true,
+      "Mèo Aegean có xu hướng thông minh và hoạt bát, với kỹ năng giao tiếp tốt. Chúng thường thân thiện với mọi người và có thể là những con mèo tuyệt vời cho các gia đình có trẻ em.",
+    inCart: false,
     count: 0,
     total: 0
   },
-  
+  {
+    id: 2,
+    type: "Mèo Mỹ đuôi cộc",
+    img: "img/cat-2.png",
+    price: 16,
+    origin: "America",
+    info:
+      "Những người yêu thích Bobtail ở Mỹ nói rằng thay vào đó bạn nên xem xét con mèo của chúng. chúng là một người yêu với trái tim vàng, người hết lòng vì người dân của mình, đi theo chúng, thích chơi đùa, đi bộ đẹp trên dây xích (tất nhiên là sau khi đào tạo) và luôn tươi cười chào đón khách..",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    type: "Mèo Mỹ Tai Xoắn",
+    img: "img/cat-3.png",
+    price: 8,
+    origin: "America",
+    info:
+      "Mèo Mỹ tai xoắn có đôi tai cong đặc biệt tạo thành vòng cung duyên dáng, tạo cho cô ấy một biểu cảm lanh lợi, lanh lợi. Ngoài đôi tai, mèo Mỹ tai xoắn còn được biết đến với bộ lông phẳng, mượt, có thể dài hoặc ngắn.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    type: "Mèo Mỹ Lông ngắn",
+    img: "img/cat-4.png",
+    price: 18,
+    origin: "America",
+    info:
+      "Mèo Mỹ lông ngắn dễ thích nghi và tốt bụng vẫn giữ được khả năng săn mồi, nhưng ngày nay, chúng có nhiều khả năng trở thành một người bạn đồng hành trong gia đình, một công việc mà chúng vượt trội.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    type: "Mèo Mỹ Lông Sợi",
+    img: "img/cat-5.png",
+    price: 24,
+    origin: "America",
+    info:
+      "Mèo Mỹ lông sợi được phân biệt với các giống khác bởi bộ lông dày và rậm, được mô tả là có cảm giác giống như len thép hoặc len cừu. Có một số mức độ héo, thay đổi từ nhọn đến xoăn, với các sợi lông riêng lẻ được uốn, nối hoặc uốn cong.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    type: "Mèo Bali",
+    img: "img/cat-6.png",
+    price: 17,
+    origin: "Bali",
+    info:
+      "Mèo Bali thường hòa hợp với tâm trạng của bạn và sẽ ở ngay để cổ vũ bạn nếu bạn buồn hoặc chia sẻ niềm vui khi bạn vui. Bản thân chúng có thể phản ứng với giọng nói của bạn và giọng điệu mắng mỏ có thể làm tổn thương cảm xúc nhạy cảm của chúng. Một giọng điệu huấn luyện và củng cố tích cực là cách hiệu quả duy nhất để điều chỉnh hành vi không mong muốn.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    type: "Mèo Bengal",
+    img: "img/cat-7.png",
+    price: 30,
+    origin: "Califonia",
+    info:
+      "Mèo Bengal lấy tên của chúng từ tên khoa học của mèo báo châu Á, Felis Bengalensis. Chúng được tạo ra thông qua con lai giữa một con mèo báo châu Á, trong những năm 1950 và những năm 1960 có thể được mua tại các cửa hàng thú cưng, và những chiếc ghế ngắn trong nước.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    type: "Mèo Birman",
+    img: "img/cat-8.png",
+    price: 2,
+    origin: "Myanmar",
+    info:
+      "Giống mèo khỏe mạnh, sống lâu này có một bộ lông mềm mượt bán dài nổi bật và một chiếc đuôi cọ dài sang trọng. Đôi mắt xanh gần như tròn sáng rực rỡ là đặc điểm nổi bật trong biểu cảm ngọt ngào.",
+    inCart: false,
+    count: 0,
+    total: 0
+  }
 ];
 
-export const detailProduct = {
+export const detailProduct_Item = {
   id: 1,
-  title: "",
-  img: "img/product-1.png",
-  price: 10,
-  company: "",
-  info:
-    "",
-  inCart: false,
-  count: 0,
-  total: 0
+    type: "Mèo Aegean Hi Lạp",
+    img: "img/cat-1.png",
+    price: 10,
+    origin: "Greek",
+    info:
+      "Mèo Aegean có xu hướng thông minh và hoạt bát, với kỹ năng giao tiếp tốt. Chúng thường thân thiện với mọi người và có thể là những con mèo tuyệt vời cho các gia đình có trẻ em.",
+    inCart: false,
+    count: 0,
+    total: 0
 };
+export const InfoOfShop = {
+  name:"Cat Boutique",
+  img:'img/shop-img.png',
+  address:"D9, Hanoi university of Science and Technology",
+  phone:" 012 345 6789",
+  time:'Mở cửa:7h - 20h',
+}
